614f93b3a6cb1ab4d4896543d89d68bc43af0d71
AleksaStoiljkovic-IM-115-18 <93438528+AleksaStoiljkovic-IM-115-18@users.noreply.github.com>
2022-05-03 12:57:15 +0200

    Backend Final

---
+++ rpp_projekat/src/main/java/rpp_projekat/repository/ProjekatRepository.java
@@ -0,0 +1,16 @@
+package rpp_projekat.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import rpp_projekat.model.Grupa;
+import rpp_projekat.model.Projekat;
+
+public interface ProjekatRepository extends JpaRepository<Projekat, Integer>{
+	
+	List<Projekat> findByNazivContainingIgnoreCase(String naziv);
+	
+	List<Projekat> findByOznakaContainingIgnoreCase(String oznaka);
+
+}
